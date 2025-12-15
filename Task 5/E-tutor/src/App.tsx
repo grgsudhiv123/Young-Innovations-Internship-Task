1eb639b4ae4321bc16f83985562f66d9f2db12da
grgsudhiv123 <grgsudeep667@gmail.com>
2025-12-15 10:30:51 +0545

    feat : new project set with folder structure

---
+++ Task 5/E-tutor/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-full bg-white">
+        <h1>New project</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
